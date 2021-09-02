73749fec3b962f0f20e65ba04cf7848537a0b3d4
thaoktk <tranthuthao.bhn.96@gmail.com>
2021-09-02 16:14:07 +0700

    update-9

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
 // header
-const headerItem = document.querySelectorAll('.header-item')
-const headerItemActive = document.querySelector('.header-item.active')
-const headerMenu = document.querySelector('.header-menu')
-const headerClose = document.querySelector('.header-close')
-const headerList = document.querySelector('.header-list-wrap')
-const headerModal = document.querySelector('.header-modal')
-const headerItemContact = document.querySelector('.header-item.header-item--contact')
+const headerList = document.querySelector('.header-list-wrap'), 
+headerModal = document.querySelector('.header-modal')
 
-// modal
-const homeViewAll = document.querySelector('.home-work__view-all')
-const homeWorkModal = document.querySelector('.modal-wrap')
-const homeWorkModalContainer = document.querySelector('.home-work__modal')
-const homeWorkClose = document.querySelector('.home-work__close')
-
-// image rotate
-const imageText = document.querySelector('.image-wrap__text')
-
-// tab pane
-const tabPanes = document.querySelectorAll('.tab-pane')
-const tabPaneContact = document.querySelector('.tab-pane.tab-pane--contact')
-
-// progress bar 
-const progress = document.querySelectorAll('.progress-bar__sub-progress')
-const projectFilterItem = document.querySelectorAll('.project-filter__item')
-const projectCategoryItem = document.querySelectorAll('.project-category__item-wrap')
-
-// hire me
-const hireMe = document.querySelector('.sub-section-item--contact')
-
+/** ===================================== FUNCTION GLOBAL ====================================================== */
 function removeActive(classRemoved) {
     classRemoved.classList.remove("active")
 }
 
 function addActive(classAdded) {
-    classAdded.classList.add('active')
+    classAdded.classList.add("active")
 }
 
 function toggleMenuMobile() {
@@ -41,137 +16,181 @@ function toggleMenuMobile() {
     removeActive(headerModal)
 }
 
-// navbar click show on 
-headerItem.forEach((item, index) => {
-    const tabPane = tabPanes[index]
-    item.onclick = function() {
-        const headerItemActive = document.querySelector('.header-item.active')
-        removeActive(headerItemActive)
-        addActive(this)
 
-        const tabPaneActive = document.querySelector('.tab-pane.active')
-        removeActive(tabPaneActive)
-        addActive(tabPane)
-
-        toggleMenuMobile()
-
-        if (item.classList.contains('header-item--skill')) { // ktra xem true hay false
-            progress.forEach(item => addActive(item))
-        } else {
-            progress.forEach(item => removeActive(item))
+/** ===================================== NAVBAR CLICK SHOW ON ====================================================== */
+;(() => {
+    const headerItem = document.querySelectorAll('.header-item'),
+    tabPanes = document.querySelectorAll('.tab-pane')
+    
+    headerItem.forEach((itemHeader, index) => {
+        const tabPane = tabPanes[index]
+        itemHeader.onclick = () => {
+            const headerItemActive = document.querySelector('.header-item.active'),
+            tabPaneActive = document.querySelector('.tab-pane.active')
+    
+            removeActive(headerItemActive)
+            addActive(itemHeader)
+            
+            removeActive(tabPaneActive)
+            addActive(tabPane)
+            
+            toggleMenuMobile()
+    
+            // run progress bar
+            const progress = document.querySelectorAll('.progress-bar__sub-progress')
+            if (itemHeader.classList.contains('header-item--skill')) { // ktra xem true hay false
+                progress.forEach(itemProgress => addActive(itemProgress))
+            } else {
+                progress.forEach(itemProgress => removeActive(itemProgress))
+            }
         }
+    
+    })
+})()
+
+
+/** ===================================== MENU MOBILE ====================================================== */
+;(() => {
+    const headerMenu = document.querySelector('.header-menu'),
+    headerClose = document.querySelector('.header-close')
+
+    headerMenu.onclick = () => {
+        addActive(headerList)
+        addActive(headerModal)
     }
-
-})
-
-// menu mobile
-headerMenu.onclick = function() {
-    addActive(headerList)
-    addActive(headerModal)
-}
-
-headerClose.addEventListener('click', toggleMenuMobile)
-
-// modal view all
-homeViewAll.onclick = function() {
-    addActive(homeWorkModal)
-}
-
-homeWorkClose.onclick = function(e) {
-    removeActive(homeWorkModal)
-    e.stopPropagation()
-}
-
-homeWorkModal.onclick = function() {
-    removeActive(homeWorkModal)
-}
-
-homeWorkModalContainer.onclick = function(e) {
-    e.stopPropagation()
-}
-
-// carousel
-$('.home-work__carousel').slick({
-    slidesToShow: 3,
-    autoplaySpeed : 2000,
-    autoplay: true,
-    infinite: true,
-    arrows:false,
-    accessibility: false,
-    responsive: [
-        {
-            breakpoint: 1025,
-            settings: {
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 740,
-            settings: {
-                slidesToShow: 1,
-            }
-        }
-    ]
-})
+    headerClose.addEventListener('click', toggleMenuMobile)
+})()
 
 
-$('.home-work__modal').slick({
-    slidesToShow: 1,
-    autoplaySpeed : 3000,
-    autoplay: true,
-    infinite: true,
-    arrows:false,
-    centerMode: true,
-    responsive: [
-        {
-            breakpoint: 740,
-            settings: {
-                slidesToShow: 1,
-                centerMode: false,
-            }
-        }
-    ]
-})
+/** ===================================== MODAL VIEW ALL ====================================================== */
+;(() => {
+    const homeViewAll = document.querySelector('.home-work__view-all'),
+    homeWorkClose = document.querySelector('.home-work__close'),
+    homeWorkModal = document.querySelector('.modal-wrap'),
+    homeWorkModalContainer = document.querySelector('.home-work__modal')
 
-// image rotate animate
-imageText.innerHTML = imageText.innerText.split("").map(
-    (char, i) => `<span style="transform: rotate(${i * 9.5}deg)">${char}</span>`
-).join("")
-
-
-// project filter
-projectFilterItem.forEach((itemFilter) => {
-    const itemFilterValue = itemFilter.getAttribute('data-filter')
-
-    if (itemFilterValue === 'all') {
-        projectCategoryItem.forEach(itemCategory => addActive(itemCategory))
+    homeViewAll.onclick = () => {
+        addActive(homeWorkModal)
     }
     
-    itemFilter.onclick = function() {
-        const projectFilterActive = document.querySelector('.project-filter__item.active')
-        removeActive(projectFilterActive)
-        addActive(this)
-
-        projectCategoryItem.forEach(itemCategory => {
-            if (itemFilterValue === 'all' || itemCategory.getAttribute('data-category') === itemFilterValue) {
-                addActive(itemCategory)
-            } else {
-                removeActive(itemCategory)
-            }
-        })
+    homeWorkClose.onclick = (e) => {
+        removeActive(homeWorkModal)
+        e.stopPropagation()
     }
-})
+    
+    homeWorkModal.onclick = () => {
+        removeActive(homeWorkModal)
+    }
+    
+    homeWorkModalContainer.onclick = (e) => {
+        e.stopPropagation()
+    }
+})()
 
-//  hire me
-hireMe.onclick = function() {
-    const headerItemActive = document.querySelector('.header-item.active')
-    removeActive(headerItemActive)
-    addActive(headerItemContact)
 
-    const tabPaneActive = document.querySelector('.tab-pane.active')
-    removeActive(tabPaneActive)
+/** ===================================== CAROUSEL VIEW ALL AND MODAL ====================================================== */
+;(() => {
+    // carousel view all
+    $('.home-work__carousel').slick({
+        slidesToShow: 3,
+        autoplaySpeed : 2000,
+        autoplay: true,
+        infinite: true,
+        arrows:false,
+        accessibility: false,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 740,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    })
 
-    setTimeout(function() {
-        addActive(tabPaneContact)
-    }, 100)
-}
+    // carousel modal
+    $('.home-work__modal').slick({
+        slidesToShow: 1,
+        autoplaySpeed : 3000,
+        autoplay: true,
+        infinite: true,
+        arrows:false,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 740,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: false,
+                }
+            }
+        ]
+    })
+})()
+
+
+/** ===================================== IMAGE ROTATE ANIMATE ====================================================== */
+;(() => {
+    const imageText = document.querySelector('.image-wrap__text')
+
+    imageText.innerHTML = imageText.innerText.split("").map(
+        (char, i) => `<span style="transform: rotate(${i * 9.5}deg)">${char}</span>`
+    ).join("")
+})()
+
+
+/** ===================================== PROJECT FILTER ====================================================== */
+;(() => {
+    const projectFilterItem = document.querySelectorAll('.project-filter__item')
+    const projectCategoryItem = document.querySelectorAll('.project-category__item-wrap')
+
+    projectFilterItem.forEach((itemFilter) => {
+        const itemFilterValue = itemFilter.getAttribute('data-filter')
+    
+        if (itemFilterValue === 'all') {
+            projectCategoryItem.forEach(itemCategory => addActive(itemCategory))
+        }
+        
+        itemFilter.onclick = () => {
+            const projectFilterActive = document.querySelector('.project-filter__item.active')
+            removeActive(projectFilterActive)
+            addActive(itemFilter)
+    
+            projectCategoryItem.forEach(itemCategory => {
+                if (itemFilterValue === 'all' || itemCategory.getAttribute('data-category') === itemFilterValue) {
+                    addActive(itemCategory)
+                } else {
+                    removeActive(itemCategory)
+                }
+            })
+        }
+    })
+})()
+
+
+/** ===================================== HIRE ME ====================================================== */
+;(() => {
+    const hireMe = document.querySelector('.sub-section-item--contact')
+
+    hireMe.onclick = () => {
+        const headerItemActive = document.querySelector('.header-item.active')
+        const headerItemContact = document.querySelector('.header-item.header-item--contact')
+        const tabPaneActive = document.querySelector('.tab-pane.active')
+        const tabPaneContact = document.querySelector('.tab-pane.tab-pane--contact')
+        
+        removeActive(headerItemActive)
+        addActive(headerItemContact)
+    
+        removeActive(tabPaneActive)
+    
+        setTimeout(() => {
+            addActive(tabPaneContact)
+        }, 100)
+    }    
+})()
